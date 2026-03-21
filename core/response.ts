d966c74e52247cd133002468ca35be6af78df296
phantekzy <mainilotfi@gmail.com>
2026-03-21 10:38:38 +0100

    ArcResponse interface

--- core/response.ts
+++ core/response.ts
@@ -1,3 +1,7 @@
 import { ServerResponse } from "http";
 
-export interface ArcResponse extends ServerResponse {}
+export interface ArcResponse extends ServerResponse {
+  status(code: number): this;
+  json(data: any): void;
+  send(data: any): void;
+}
