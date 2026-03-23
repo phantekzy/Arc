cafb91a8829e379e021ccb410cdebe1e3029e14b
phantekzy <mainilotfi@gmail.com>
2026-03-23 12:38:11 +0100

    middleware

--- core/app.ts
+++ core/app.ts
@@ -1,5 +1,9 @@
-import { Router } from "./router";
+import { Middleware, Router } from "./router";
 
 export class Arc {
   public router = new Router();
+  private middlewares: Middleware[] = [];
+  use(m: Middleware) {
+    this.middlewares.push(m);
+  }
 }
