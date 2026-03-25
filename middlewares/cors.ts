2efc2d9bf2d914186f59ad72859f10eb45a8e771
phantekzy <mainilotfi@gmail.com>
2026-03-25 09:37:35 +0100

    cors finished

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -2,5 +2,10 @@ import { Middleware } from "../core/router";
 
 export const cors: Middleware = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  if (req.method === "OPTIONS") {
+    res.status(204).end();
+    return;
+  }
+  next();
 };
