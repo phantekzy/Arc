74ddd7fcdb0ac9aa87ae8b3cf8670822058410c0
phantekzy <mainilotfi@gmail.com>
2026-03-25 08:53:15 +0100

    Body parser done

--- middlewares/jsonParser.ts
+++ middlewares/jsonParser.ts
@@ -1,5 +1,14 @@
 import { Middleware } from "../core/router";
-
 export const jsonParser: Middleware = (req, res, next) => {
   if (req.headers["content-type"] !== "application/json") return next();
+  let data = "";
+  req.on("data", (c) => (data += c));
+  req.on("end", () => {
+    try {
+      req.body = data ? JSON.parse(data) : {};
+      next();
+    } catch {
+      next(new Error("Invalid JSON"));
+    }
+  });
 };
