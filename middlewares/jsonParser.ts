a95b63e3ab2dceec030ae60ce99dd786958aa22a
phantekzy <mainilotfi@gmail.com>
2026-03-25 08:42:17 +0100

    application/json condition

--- middlewares/jsonParser.ts
+++ middlewares/jsonParser.ts
@@ -1,3 +1,5 @@
 import { Middleware } from "../core/router";
 
-export const jsonParser: Middleware = (req, res, next) => {};
+export const jsonParser: Middleware = (req, res, next) => {
+  if (req.headers["content-type"] !== "application/json") return next();
+};
