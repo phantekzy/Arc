2866ac0894570b85fc31400c1879b77b8d3e7368
phantekzy <mainilotfi@gmail.com>
2026-03-26 13:59:21 +0100

    filtering the request

--- middlewares/urlencodedParser.ts
+++ middlewares/urlencodedParser.ts
@@ -2,4 +2,8 @@ import { Middleware } from "../core/router";
 
 const MAX_PAYLOAD_SIZE = 1048576;
 
-export const urlencodedParser: Middleware = (req, res, next) => {};
+export const urlencodedParser: Middleware = (req, res, next) => {
+  if (req.headers["content-type"] !== "application/x-www-form-urlencoded") {
+    return next();
+  }
+};
