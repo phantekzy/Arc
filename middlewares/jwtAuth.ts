c57b478559282a0983d7d7f1db1495e2ee2046ee
phantekzy <mainilotfi@gmail.com>
2026-03-26 12:42:42 +0100

    jwtAuth done

--- middlewares/jwtAuth.ts
+++ middlewares/jwtAuth.ts
@@ -1,7 +1,14 @@
-import { HttpError } from "../core/error";
+import jwt from "jsonwebtoken";
 import { Middleware } from "../core/router";
+import { HttpError } from "../core/error";
 
 export const jwtAuth: Middleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return next(new HttpError(401, "Unauthorized"));
+  try {
+    req.user = jwt.verify(token, "SECRET_KEY");
+    next();
+  } catch {
+    next(new HttpError(403, "Invalid Token"));
+  }
 };
