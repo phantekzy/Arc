2105a4fc22cd080e4c2eaa0198033dbc9b426928
phantekzy <mainilotfi@gmail.com>
2026-03-27 06:29:16 +0100

    cookies

--- middlewares/cookieParser.ts
+++ middlewares/cookieParser.ts
@@ -3,4 +3,7 @@ import { Middleware } from "../core/router";
 export const cookieParser: Middleware = (req, res, next) => {
   const cookieHeader = req.headers.cookie;
   req.cookies = {};
+
+  if (!cookieHeader) return next();
+  const cookies = cookieHeader.split(";");
 };
