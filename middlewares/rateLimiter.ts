13cd318aaca4831a1b879c6759d9a3e5cae99080
phantekzy <mainilotfi@gmail.com>
2026-03-27 18:50:30 +0100

    added a timestamp then we check our store to see if we have seen this IP before

--- middlewares/rateLimiter.ts
+++ middlewares/rateLimiter.ts
@@ -10,5 +10,7 @@ const store = new Map<string, RateRecord>();
 export const rateLimiter = (limit: number, windowMs: number): Middleware => {
   return (req, res, next) => {
     const ip = req.socket.remoteAddress || "unknown";
+    const now = Date.now();
+    const record = store.get(ip);
   };
 };
