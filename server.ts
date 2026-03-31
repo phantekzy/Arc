f3bc6e1148ec1024cac3ba471e91e6e8f0d7fc71
phantekzy <mainilotfi@gmail.com>
2026-03-31 17:34:36 +0100

    added loggins and security bounds and all the parses i created

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import { Arc } from "./core/app";
 import { cookieParser } from "./middlewares/cookieParser";
 import { cors } from "./middlewares/cors";
+import { jsonParser } from "./middlewares/jsonParser";
 import { logger } from "./middlewares/logger";
 import { rateLimiter } from "./middlewares/rateLimiter";
+import { urlencodedParser } from "./middlewares/urlencodedParser";
 
 const app = new Arc();
 
@@ -10,3 +12,5 @@ app.use(logger);
 app.use(cors);
 app.use(rateLimiter(100, 60000));
 app.use(cookieParser);
+app.use(jsonParser);
+app.use(urlencodedParser);
