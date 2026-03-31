81995248149bfa97291833e5f4c25369fc035ca3
phantekzy <mainilotfi@gmail.com>
2026-03-31 10:27:35 +0100

    During development, we can warn. In production, we MUST halt.

--- utils/tokenUtils.ts
+++ utils/tokenUtils.ts
@@ -1,1 +1,11 @@
 const SECRET = process.env.JWT_SECRET;
+
+if (!SECRET) {
+  if (process.env.NODE_ENV === "production") {
+    throw new Error(
+      "FATAL: JWT_SECRET is not defined in production environment.",
+    );
+  } else {
+    console.warn("WARNING: JWT_SECRET is missing. Using insecure dev fallback");
+  }
+}
