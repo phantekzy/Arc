f082e43b065f8ba79d98fd2139c8bc5582147623
phantekzy <mainilotfi@gmail.com>
2026-03-31 17:10:26 +0100

    added global middlewares

--- server.ts
+++ server.ts
@@ -1,4 +1,11 @@
 import "dotenv/config";
 import { Arc } from "./core/app";
+import { logger } from "./middlewares/logger";
+import { cors } from "./middlewares/cors";
+import { jsonParser } from "./middlewares/jsonParser";
 
 const app = new Arc();
+
+app.use(logger);
+app.use(cors);
+app.use(jsonParser);
