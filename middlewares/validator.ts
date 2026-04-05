731fce563be975d1406a64e85b92f35fd0cb248a
phantekzy <mainilotfi@gmail.com>
2026-04-05 09:21:55 +0100

    validator done

--- middlewares/validator.ts
+++ middlewares/validator.ts
@@ -22,5 +22,9 @@ export const validate = (schema: Record<string, string>): Middleware => {
         );
       }
     }
+    if (errors.length > 0) {
+      return res.status(400).json({ status: "error", errors });
+    }
+    next();
   };
 };
