3bbd7605061cb385a8b9c2d611d75d73a42e5a2b
phantekzy <mainilotfi@gmail.com>
2026-04-05 09:19:18 +0100

    check if the key exists

--- middlewares/validator.ts
+++ middlewares/validator.ts
@@ -4,12 +4,17 @@ export const validate = (schema: Record<string, string>): Middleware => {
   return (req, res, next) => {
     const body = req.body;
     if (!body || typeof body !== "object" || Array.isArray(body)) {
-      return res
-        .status(400)
-        .json({
-          error: "Validation Failed",
-          message: "JSON body is required.",
-        });
+      return res.status(400).json({
+        error: "Validation Failed",
+        message: "A valid JSON object is required.",
+      });
+    }
+    const errors: string[] = [];
+    for (const [key, expectedType] of Object.entries(schema)) {
+      if (!(key in body)) {
+        errors.push(`Field '${key}' is missing`);
+        continue;
+      }
     }
   };
 };
